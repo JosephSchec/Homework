f422cd7d9581e6547170351563ff35fe2cbecde2
Joseph-Schechter <shecky522@gmail.com>
2021-08-19 11:37:31 -0400

    homework

---
+++ JavaScript/Homework 64/module1.js
@@ -0,0 +1,9 @@
+
+window.myApp = window.myApp || {};
+window.myApp.Utils = (function (fillMyApp) {
+    'use strict';
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Shabbos'];
+    fillMyApp.dayToNumber = (dayOfWeek) => weekDays.findIndex(wd => wd.toLowerCase() === dayOfWeek.toLowerCase()) + 1,
+    fillMyApp.numberToDay = n => weekDays[n - 1];
+    return fillMyApp;
+}(window.myApp || {}));
---
+++ JavaScript/Homework 64/module2.js
@@ -0,0 +1,6 @@
+window.myApp = window.myApp || {};
+window.myApp.Utils = (function (fillMyApp) {
+    'use strict';
+    fillMyApp.stringCaseInsensitiveEquals=(string1,string2)=>string1.toLowerCase()===string2.toLowerCase();
+    return fillMyApp;
+}(window.myApp || {}));
