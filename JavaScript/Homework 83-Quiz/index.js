c4e208baa45f4025843f3103cb50d1574b8521ab
slubowsky <270589+slubowsky@users.noreply.github.com>
2021-12-06 16:30:02 -0500

    quiz comments

--- JavaScript/Homework 83-Quiz/index.js
+++ JavaScript/Homework 83-Quiz/index.js
@@ -32,12 +32,15 @@
             }
             const orders = await store.json();
 
+            // SL - theres a little to much going on here. I would first load the items, then add them to dom in a separate function
+            // Also your orders dont have Items in them, they have the original simple objects from json. You just use Item when adding it to dom
             orders.forEach(order => {
                 const theOrder = new Order(order.customer, order.address, order.items);
                 $(`<div></div><hr><h3> Customer: ${theOrder.cName}</h3>
                 <p>Address: ${theOrder.cAddress}</p>
                 <p>Total : $${theOrder.total}</p><h3>Items</h3>`).appendTo('body');
                 order.items.forEach(item => {
+                    // SL - theItems? interesting name for a single item.
                     const theItems = new Item(item.item, (item.total / item.quantity), item.quantity);
                     $(`<p>Item: ${theItems.name}</p>
                      <p>Quantity: ${theItems.quantity}</p>
@@ -51,3 +54,5 @@
 
 
 }());
+
+// SL - nice - 95
